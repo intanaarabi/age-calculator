f78ac1415f5d6dcb3ac3e7a16c71a62468473821
Dayang Intan <intan.abangarabi@gmail.com>
2023-11-10 23:26:13 +0800

    Setup foundational components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import AgeCalculator from './components/AgeCalculator'
 
 function App() {
 
   return (
     <>
-      
+      <div className='min-h-screen bg-neutral-off-white flex flex-col justify-center items-center'>
+        <AgeCalculator/>
+      </div>
     </>
   )
 }
---
+++ src/components/AgeCalculator.jsx
@@ -0,0 +1,9 @@
+function AgeCalculator() {
+    return (
+        <div className='bg-neutral-white p-20'> 
+        
+        </div>
+    )
+}
+
+export default AgeCalculator
