133850f7842f06e5f46f7a62f26345915f34b3a0
Dayang Intan <intan.abangarabi@gmail.com>
2023-11-10 23:22:04 +0800

    Update custom tailwind color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,21 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          purple: 'hsl(259, 100%, 65%)',
+          'light-red': 'hsl(0, 100%, 67%)',
+        },
+        neutral: {
+          white: 'hsl(0, 0%, 100%)',
+          'off-white': 'hsl(0, 0%, 94%)',
+          'light-grey': 'hsl(0, 0%, 86%)',
+          'smokey-grey': 'hsl(0, 1%, 44%)',
+          'off-black': 'hsl(0, 0%, 8%)',
+        },
+      },
+    },
   },
   plugins: [],
 }
